b117a7e0eb14ed3ec0b8a8fcc0a691f369687aff
rakeshwasnik <rbwasnik99@gmail.com>
2022-10-16 13:56:45 +0530

    .

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# NetNinja
+React Practice
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+
+  const name = 'Welcome to the App';
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>{name}</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
