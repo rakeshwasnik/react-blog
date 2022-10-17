85e62b761a541850630ac4bab8354b4a1220e5ee
rakeshwasnik <rbwasnik99@gmail.com>
2022-10-17 16:30:35 +0530

    .

--- src/Home.js
+++ src/Home.js
@@ -5,14 +5,19 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'Rakesh', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Rakesh2', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Rakesh3', id: 3 }
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'notRakesh', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Rakesh', id: 3 }
       ])
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id != id);
+        setBlogs(newBlogs);
+    }
     
     return ( 
 
         <div className="home">
-            <BlogList blogs={blogs} title={"All Blogs!"} />
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
         </div>
      );
 }
