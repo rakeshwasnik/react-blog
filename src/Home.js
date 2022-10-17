0729a703151eaf0919137a5fec8939a19b2880c7
rakeshwasnik <rbwasnik99@gmail.com>
2022-10-17 18:49:09 +0530

    .

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -9,15 +9,24 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Rakesh', id: 3 }
       ])
 
+    const [name, setName] = useState('Rakesh');
+
     const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id != id);
+        const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
+
+    useEffect(() => {
+        console.log('use effect ran');
+        console.log(name);
+    }, [name]);  //twice runs useEffect as twice rendered: newer version of React has StrictMode which ran it twice on development environment for debugging purpose, it will be once in case of production
     
     return ( 
 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+            <button onClick={() => setName('Rakesh2')}>Change Name</button>
+            <p>{ name }</p>
         </div>
      );
 }
